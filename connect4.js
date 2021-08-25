07b7948276adc818652a5a1eccb33e75037449ff
Colin Sidberry <colinsidberry@colins-mbp.lan>
2021-08-25 14:46:54 -0700

    updated makeBoard function

--- connect4.js
+++ connect4.js
@@ -16,10 +16,17 @@ const board = []; // array of rows, each row is array of cells  (board[y][x])
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
-
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for(let i = 0; i< HEIGHT; i++){
+    let column = []
+    for(let j = 0; j < WIDTH; j++){
+      column.push(null);
+    }
+    board.push(column);
+  }
 }
+
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
@@ -146,4 +153,6 @@ function checkForWin() {
 }
 
 makeBoard();
+console.log(board);
+
 makeHtmlBoard();
